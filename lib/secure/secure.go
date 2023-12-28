8eecedcae27d8efbd6bdf5f0eb0a8f2905877fa3
Yad Smood <y.s.inside@gmail.com>
2023-12-28 23:59:38 +0800

    up

--- lib/secure/secure.go
+++ lib/secure/secure.go
@@ -1,23 +1,24 @@
 // Package secure makes encrypted data can only be decrypted by selected receivers.
 // It allows different types of public keys to secretly exchange data.
 //
-// Suppose if we have a key pair X, X0 represents its private key, X1 represents its public key.
+// Suppose we have a opponent X, X0 represents its private key, X1 represents its public key.
 // We have a pool of key pairs S, they accessible by everyone, they are pregenerated key pairs
 // with the combinations of commonly use key types and sizes, such as 1024bit rsa 1024, 2048bit rsa, 256bit ecdsa, etc.
-// Now we have X and Y, they are different key type, such as X is rsa, Y is ecdsa,
-// we want to encrypt data with X and decrypt it with Y. X has access to Y1.
+// Now we have opponents X and Y, they may have different key types, such as X's is rsa, Y's is ecdsa,
+// we want to encrypt data D with X and decrypt it with Y. X has access to Y1.
 //
 // Encryption steps:
 //
 //	Find M0 from S that has the same type of Y1.
-//	Use Y1 and M0 to generate the shared 256bit AES secret key K.
-//	Encrypt the data with K.
+//	Use Y1 and M0 to generate the shared secret key K.
+//	Use K to encrypt the D to encrypted data E.
+//	Send K and E to Y.
 //
 // Decryption steps:
 //
 //	Find M1 from S that has the same type of Y0.
-//	Use Y0 and M1 to generate the shared 256bit AES secret key K.
-//	Decrypt the data with K.
+//	Use Y0 and M1 to generate the shared secret key K.
+//	Use K to decrypt E.
 package secure
 
 import (
