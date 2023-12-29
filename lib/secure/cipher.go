677dd49b7dca4b02e7a1c29e594d81f8569a6262
Yad Smood <y.s.inside@gmail.com>
2023-12-29 16:05:44 +0800

    up

--- lib/secure/cipher.go
+++ lib/secure/cipher.go
@@ -9,6 +9,17 @@ import (
 	"github.com/ysmood/whisper/lib/piper"
 )
 
+// Cipher to encrypt and decrypt data.
+// The cipher will generate a random AES secret, each public key will be used to encrypt the AES secret into a key.
+// The wire format looks like this:
+//
+//	[n][key1][key2][key3]...[encrypted-data].
+//
+// [n] is the number of keys.
+// [key1] is the encrypted key for the first public key.
+// [key2] is the encrypted key for the second public key.
+// ...
+// [encrypted-data] is the encrypted data by the AES secret.
 type Cipher struct {
 	Secure *Secure
 }
@@ -17,11 +28,6 @@ func (s *Secure) Cipher() *Cipher {
 	return &Cipher{Secure: s}
 }
 
-// Encoder format is:
-//
-//	[encrypted key count][aes key 1][aes key 2]...[encrypted data].
-//
-// Each key is for a public key.
 func (c *Cipher) Encoder(w io.Writer) (io.WriteCloser, error) {
 	aesKey, encryptedKeys, err := c.Secure.AESKeys()
 	if err != nil {
